a80667d50cef520e6305a470b0b819bbe25bf5a8
Wecton <Wecton.Santos12@outlook.com>
2024-10-25 08:28:51 -0300

    musica

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.getElementById("popup");
+    const popupName = document.getElementById("popup-name");
+    const popupInfo = document.getElementById("popup-info");
+    const closeBtn = document.querySelector(".close-btn");
+    const audio = document.getElementById("audio");
+
+    const pessoas = [
+        {
+            nome: "Pedro Henrique Morereira",
+            info: "Programador",
+            musica: "musicas/tony-country.mp3" 
+        },
+        {
+            nome: "Wecton Santos",
+            info: "Programador",
+            musica: "musicas/wecton.mp3" 
+        }
+    ];
+
+    document.querySelectorAll('.imgPedro, .imgWecton').forEach((img, index) => {
+        img.addEventListener('click', () => {
+            popupName.textContent = pessoas[index].nome;
+            popupInfo.textContent = pessoas[index].info;
+            popup.style.display = "block";
+
+            audio.src = pessoas[index].musica;
+            audio.currentTime = 0; 
+            audio.play(); 
+        });
+    });
+
+    closeBtn.addEventListener("click", function() {
+        popup.style.display = "none";
+        audio.pause(); 
+    });
+
+    window.addEventListener("click", function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+            audio.pause(); 
+        }
+    });
+});
